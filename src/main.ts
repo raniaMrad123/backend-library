29bb67e0c4c5d38799c82de4f11e78c8ed1c85de
Rahma Hdhili <rahma1.benhdhili@gmail.com>
2026-04-24 16:28:29 +0100

    favoris creation

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  });
+  app.enableCors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+   });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,6 +17,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+
+await app.listen(3001); 
 }
 bootstrap();
